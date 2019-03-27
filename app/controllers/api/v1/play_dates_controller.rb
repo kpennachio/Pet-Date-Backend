7e23beb5db971c15768725b71048578e5f94baa3
cvolpe12 <christopher.volpe12@gmail.com>
2019-03-27 15:47:34 -0400

    front end pet can now get posts and play dates

--- app/controllers/api/v1/play_dates_controller.rb
+++ app/controllers/api/v1/play_dates_controller.rb
@@ -6,6 +6,15 @@ class Api::V1::PlayDatesController < ApplicationController
     render json: @play_dates
   end
 
+  def create
+    @play_date = PlayDate.new(play_date_params)
+    if @play_date.save
+      render json: @play_date, status: :accepted
+    else
+      render json: { errors: @play_date.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   def update
     @play_date.update(play_date_params)
     if @play_date.save
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,3 +1,6 @@
 class PetSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :species, :breed, :hobbies, :plays_well, :picture, :bio
+  has_many :sent_requests
+  has_many :received_requests
+  has_many :posts
 end
